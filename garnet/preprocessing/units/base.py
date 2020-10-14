baae5bb91dac754a0056343ecb1410dde5ebb05b
databinQ <blessbingo@163.com>
2020-10-14 23:55:05 +0800

    feat(preprocessing/units): basic Unit class and tokenizer

---
+++ garnet/preprocessing/units/base.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+"""
+@File   : base.py
+@Author : garnet
+@Time   : 2020/10/14 23:11
+"""
+
+import abc
+
+
+class Unit(metaclass=abc.ABCMeta):
+    r"""Data/text process unit without states, in other words it does not need fitting.
+    """
+
+    @abc.abstractmethod
+    def transform(self, inputs, *args, **kwargs):
+        ...
+
+
+class StatefulUnit(Unit, metaclass=abc.ABCMeta):
+    r"""Data/text process unit with states. Need to be fitted before transformation. All states will be gathered during
+    fitting process.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.fitted = False
+
+    def fit(self, inputs, *args, **kwargs):
+        self.fitted = True
+
+    def reverse_transform(self, inputs, *args, **kwargs):
+        ...
---
+++ garnet/preprocessing/units/tokenizer.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+"""
+@File   : tokenizer.py
+@Author : garnet
+@Time   : 2020/10/14 23:50
+"""
+
+from .base import StatefulUnit
+
+
+class BaseTokenizer(StatefulUnit):
+    pass
