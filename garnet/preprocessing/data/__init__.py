a426f03abe1b2a701fe0a11737cca09dec1515d1
garnetow <69332503+garnetow@users.noreply.github.com>
2020-10-21 08:34:32 +0800

    feat(preprocessing/data): common import

--- garnet/preprocessing/data/__init__.py
+++ garnet/preprocessing/data/__init__.py
@@ -5,3 +5,31 @@
 @Author : garnet
 @Time   : 2020/9/9 14:48
 """
+
+from .dataset import Dataset
+from .dataset import DatasetKind
+from .dataset import MappingDataset
+from .dataset import IterableDataset
+from .dataset import MatrixDataset
+
+from .sampler import Sampler
+from .sampler import MappingSampler
+from .sampler import InfiniteStreamSampler
+from .sampler import SequentialSampler
+from .sampler import RandomSampler
+from .sampler import BatchSampler
+
+from .collator import Collator
+from .collator import IdenticalCollator
+from .collator import SingleSampleCollator
+from .collator import BatchSampleCollator
+
+from .fetcher import Fetcher
+from .fetcher import MappingFetcher
+from .fetcher import IterableFetcher
+from .fetcher import IterableBufferFetcher
+
+from .dataloader import DataLoader
+from .dataloader import BaseDataIterator
+from .dataloader import SingleProcessDataIterator
+from .dataloader import MultiProcessIterableDataIterator
