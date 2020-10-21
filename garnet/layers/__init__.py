a5b01eb82fe868068d19e05c3fe915410ebc1632
garnetow <69332503+garnetow@users.noreply.github.com>
2020-10-21 08:22:21 +0800

    feat(layer/init): common import

--- garnet/layers/__init__.py
+++ garnet/layers/__init__.py
@@ -6,4 +6,14 @@
 @Time   : 2020/9/30 16:23
 """
 
+from .core import BiasAdd
+
 from .position import PositionEmbedding
+
+from .attention import MultiHeadAttention
+
+from .embedding import DenseEmbedding
+
+from .feedforward import FeedForward
+
+from .layer_normalization import LayerNormalization
